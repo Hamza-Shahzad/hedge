3edb3070f99b846e87d28cd135ac24d04f6e5654
hamza-shahzad <hamza.ullu.4@gmail.com>
2020-12-18 18:30:07 -0500

    Project Start

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {withAuthenticator} from 'aws-amplify-react-native';
 
-export default function App() {
+
+Amplify.configure(awsconfig)
+
+async function signOut(){
+
+  try{
+    await Auth.signOut();
+  } catch(error){
+    console.log('error signing out', error);
+  }
+
+
+}
+
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Button title="Sign out" onPress={()=> signOut()} />
     </View>
   );
 }
@@ -19,3 +39,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App);
